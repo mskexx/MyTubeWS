96352c27b61f986c6068fcf6cbe57616a2d66dd1
MarcosSusin <marcos.bongocube@gmail.com>
2017-12-14 21:43:40 +0100

    Upload service basic implementation working

--- MyTubeWebserviceWeb/src/MyTubewsWeb/WebService.java
+++ MyTubeWebserviceWeb/src/MyTubewsWeb/WebService.java
@@ -57,7 +57,6 @@ public class WebService {
 	//POST Register
 	@POST
 	@Path("/user")
-	@Produces(MediaType.TEXT_PLAIN)
 	public Response registerUser(String user_data){
 		try {
 			
@@ -118,14 +117,10 @@ public class WebService {
 	public Response searchVideosByWord(@PathParam("title") String title){
 		try {
 			Statement st = getStatement();
-			System.out.println("BUSCANDO EN EL WEBSERVICE "+title);
-			String query = "SELECT title FROM mytube_videos WHERE title LIKE '%"+title+"%' OR description LIKE '%"+title+"%';";
-			System.out.println(query);
+			System.out.println("[SEARCH BY WORD] "+title);
 			ResultSet rs = st.executeQuery("SELECT title FROM mytube_videos WHERE title LIKE '%"+title+"%' OR description LIKE '%"+title+"%';");
-			System.out.println("BUSCANDO EN EL WEBSERVICE 2 "+title);
 			List<String> videos = new ArrayList<>();
 			while(rs.next()){		
-				System.out.println(rs.getString("title"));
 				videos.add(rs.getString("title"));
 			}
 			return Response.status(200).entity(videos).build();
@@ -138,12 +133,17 @@ public class WebService {
 	@GET
 	@Path("/videos/id/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response searchVideosByTitle(){
+	public Response searchVideosByTitle(@PathParam("id") String id){
 		try {
 			Statement st = getStatement();
-			ResultSet rs = st.executeQuery("SELECT username FROM mytube_users;");
-			return Response.status(400).entity("NOT IMPLEMENTED").build();
-			
+			ResultSet rs = st.executeQuery("SELECT title FROM mytube_videos WHERE id_video="+Integer.parseInt(id));
+			if(!rs.isBeforeFirst()){
+				return Response.status(401).entity("No video with ID "+ id.toString()).build();
+			}else{
+				rs.next();
+				return Response.status(200).entity(rs.getString("title")).build();
+				
+			}
 		} catch (SQLException se) {
 			return Response.status(500).entity("Database ERROR" + se.toString()).build();
 		}
@@ -152,12 +152,51 @@ public class WebService {
 	@POST
 	@Path("/videos")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response uploadVideo(){
+	public Response uploadVideo(VideoData video_data){
 		try {
+			 String title = video_data.getVid_title();
+			 String description = video_data.getVid_desc();
+			 String username = video_data.getVid_owner();
+			 String url = video_data.getVid_url();
+			 int id_server = 0;
+			 
+			System.out.println("Subiendo video");
 			Statement st = getStatement();
-			ResultSet rs = st.executeQuery("INSERT INTO mytube_videos(id_video, title, description, username_owner, id_server) "
-					+ "VALUES (?, ?, ?, ?, ?);");
-			return Response.status(400).entity("NOT IMPLEMENTED").build();
+			ResultSet rs = st.executeQuery("SELECT title FROM mytube_videos WHERE title='"+title+"';");
+			if(rs.isBeforeFirst()){
+				return Response.status(201).build(); //Video already exists
+			}
+			st = getStatement();
+			rs = st.executeQuery("SELECT id_server FROM mytube_servers WHERE url_server='"+url+"';");
+			if(!rs.isBeforeFirst()){
+				System.out.println("No existe ese servidor");
+				Connection con = getConnection();
+				PreparedStatement state = con.prepareStatement("INSERT INTO mytube_servers(url_server) VALUES (?) RETURNING id_server");
+				state.setString(1, url);
+				state.execute();
+				System.out.println("QUERY HECHA");
+				ResultSet rs_return = state.getResultSet();
+				rs_return.next();
+				id_server = rs_return.getInt("id_server");
+				System.out.println("La id del nuevo servidor es:"+ String.valueOf(id_server));
+			}
+			
+			Connection con = getConnection();
+			PreparedStatement state = con.prepareStatement("INSERT INTO mytube_videos(title, description, username_owner, id_server) VALUES (?,?,?,?) RETURNING id_video");
+			state.setString(1, title);
+			state.setString(2, description);
+			state.setString(3, username);
+			state.setInt(4, id_server);
+			state.execute();
+			
+			ResultSet new_video = state.getResultSet();
+			if(!new_video.isBeforeFirst()){
+				return Response.status(201).build(); //Video already exists
+			}
+			new_video.next();
+			int id_video = new_video.getInt("id_video");
+			System.out.println("La id del nuevo video es:" + String.valueOf(id_video));
+			return Response.status(200).entity(String.valueOf(id_video)).build();
 			
 		} catch (SQLException se) {
 			return Response.status(500).entity("Database ERROR" + se.toString()).build();
@@ -165,19 +204,37 @@ public class WebService {
 		}
 	}
 	
-	//POST (Download video)
+	//GET (Download video)
 	@GET
 	@Path("/videos/{video}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response donwloadVideo(@PathParam("video") String title){
 		try {
+			int id_server = 0;
+			String url_server;
 			Statement st = getStatement();
-			ResultSet rs = st.executeQuery("SELECT username FROM mytube_users;");
-			return Response.status(400).entity("NOT IMPLEMENTED").build();
+			ResultSet rs = st.executeQuery("SELECT id_server FROM mytube_videos WHERE title ='"+title+"';");
+			
+			if(!rs.isBeforeFirst()){
+				return Response.status(401).entity("No video with this title "+ title).build();
+			}else{
+				rs.next();
+				id_server = rs.getInt("id_server");
+			}
+			
+			st = getStatement();
+			rs = st.executeQuery("SELECT url_server FROM mytube_servers WHERE id_server ="+id_server);
+			if(!rs.isBeforeFirst()){
+				return Response.status(401).entity("No server with this ID "+ id_server).build();
+			}else{
+				rs.next();
+				url_server = rs.getString("url_server");
+			}
+			return Response.status(200).entity(url_server).build();
 			
 		} catch (SQLException se) {
 			return Response.status(500).entity("Database ERROR" + se.toString()).build();
-		}
+		} 
 	}
 	
 	//PUT (Upload video)
